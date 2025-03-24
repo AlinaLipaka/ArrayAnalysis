934bc82b8a9e8cac8fc848abd4d156b23512c864
alinalipaka <leenka013@gmail.com>
2025-03-24 19:51:23 +0200

    whitespace ignore added, user entry moved to comment

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,17 +6,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        //user entry
+        /*
+        //User entry
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter your numbers, using comma as separator:");
         String strInput = scanner.nextLine();
-        String[] strArray = strInput.split(",");
+        String[] strArray = strInput.replaceAll("\\s","").split(",");
 
         int[] numbers = Arrays.stream(strArray)
                 .mapToInt(Integer::parseInt)
                 .toArray();
+         */
 
-        //int[] numbers = {4, 5, -3, 15, -2, 19, 6, -2, -4, 20, -17};
+        int[] numbers = {4, 5, -3, 15, -2, 19, 6, -2, -4, 20, -17};
         int positive = 0, negative = 0, even = 0, odd = 0;
 
         for (int number : numbers) {
