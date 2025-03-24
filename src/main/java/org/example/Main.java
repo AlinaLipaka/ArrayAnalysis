d397f5e64099cec43bfbaa10acfe5daba75efb4c
Alina Lipaka <alinalipaka@Alinas-MacBook-Air.local>
2025-03-24 18:44:09 +0200

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,29 @@
+package org.example;
+
+
+public class Main {
+    public static void main(String[] args) {
+        int[] numbers = {4, 5, -3, 15, -2, -4, 20, -17, 0};
+        int positive = 0, negative = 0, even = 0, odd = 0;
+
+        for (int number : numbers) {
+            if (number != 0) {
+                if (ifPositive(number)) {
+                    positive++;
+                } else negative++;
+            }
+
+            if (ifEven(number)) {
+                even++;
+            } else odd++;
+        }
+        System.out.println("positive: " + positive + ", negative: " + negative + ", even: " + even + ", odd: " + odd);
+    }
+    public static Boolean ifPositive(int number){
+        return number > 0;
+    }
+
+    public static Boolean ifEven(int number){
+        return number % 2 == 0;
+    }
+}
