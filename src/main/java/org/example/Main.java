da50b56d0364705b0e6aa8e90a9d42539561c2b6
alinalipaka <leenka013@gmail.com>
2025-03-24 19:06:23 +0200

    user input scanner added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,22 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int[] numbers = {4, 5, -3, 15, -2, 19, 6, -2, -4, 20, -17};
+
+        //user entry
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your numbers, using comma as separator:");
+        String strInput = scanner.nextLine();
+        String[] strArray = strInput.split(",");
+
+        int[] numbers = Arrays.stream(strArray)
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        //int[] numbers = {4, 5, -3, 15, -2, 19, 6, -2, -4, 20, -17};
         int positive = 0, negative = 0, even = 0, odd = 0;
 
         for (int number : numbers) {
@@ -19,6 +32,7 @@ public class Main {
         }
         System.out.println("positive: " + positive + ", negative: " + negative + ", even: " + even + ", odd: " + odd);
     }
+
     public static Boolean ifPositive(int number){
         return number > 0;
     }
